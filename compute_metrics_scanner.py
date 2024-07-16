3ecdd19a2ad10dbda608959219f8adf9f5c7aaf6
moamirian <mohammadreza.amirian@gmail.com>
2024-07-16 14:25:32 +0000

    First stable pipeline for registration.

--- compute_metrics_scanner.py
+++ compute_metrics_scanner.py
@@ -24,12 +24,19 @@ registration_mode = 'elastic'#'ants'#'elastic'# 'elastic'#'elstic' #None
 
 print('Registaration method: ', registration_mode)
 
-level_window_torch = lambda x, level, window: torch.clamp((x - level + window / 2) / window, 0, 1)
+level_window_torch = lambda x, level, window: torch.clamp(x,  level - window / 2, level + window/2)
 #level_window = lambda x, level, window: np.clip((x - level + window / 2) / window, 0, 1)
 level_window = lambda x, level, window: np.clip(x, level-window/2, level+window/2)
-level, window = 0, 1000
+#level, window = 0, 1000
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def flip_volume(volume):
+    volume_flipped = np.zeros_like(volume)
+    nslides = volume.shape[0]
+    for i in range(nslides):
+         volume_flipped[i] = volume[nslides - i - 1]
+    return volume_flipped
 
 def read_dicom(dicom_dir, numpy_format=False):
     # List to hold the image arrays
@@ -45,32 +52,49 @@ def read_dicom(dicom_dir, numpy_format=False):
             # Extract the pixel array and add to the list
             slices.append(ds)
             slice_locations.append(float(ds.SliceLocation))
-
+   
+    # Sort slices by Slice Location
+    slices.sort(key=lambda x: float(x.SliceLocation))
+    
+    # Extract pixel data and stack into a 3D array
+    image_stack = np.stack([s.pixel_array for s in slices], axis=0)
+    image_stack = float(ds.RescaleSlope) * image_stack + float(ds.RescaleIntercept)
     # Sort slices by Image Position Patient (z-axis position)
     # slices.sort(key=lambda ds: ds.ImagePositionPatient[2])
 
     # Sort slices by Slice Location
-    slices = [x for _, x in sorted(zip(slice_locations, slices))]
-
+    # sorted_indices = sorted(range(len(slice_locations)), key=lambda k: slice_locations[k])
+    # slices_reoreded = [slices[i] for i in sorted_indices]
+    #slices = [x for _, x in sorted(zip(slice_locations, slices))]
+    # if ['D1', 'E1'] in dicom_dir:
+    #     volume = image_stack
+    # else:
+    #     volume = flip_volume(image_stack)
+    volume = image_stack
+    if volume.shape[0] < 343:
+        # Calculate the zoom factors for each dimension
+        zoom_factors = (343 / volume.shape[0], 1, 1)
+        # Resample the image
+        volume = zoom(volume, zoom_factors, order=3) 
+    # Crop the region of Phantom
+    volume = volume[40:280, 120:395, 64:445]
+    # idx=-76;plt.imshow(np.clip(volume, -500, 1000)[idx,...], 'gray');plt.savefig('fig.png');
     # Create a 3D numpy array from the sorted slices
-    volume = np.stack([ds.pixel_array for ds in slices], axis=0)
-    volume = float(ds.RescaleSlope) * volume + float(ds.RescaleIntercept)
-    volume = level_window(volume, 500, 3000)
+    # volume = np.stack([ds.pixel_array for ds in slices], axis=0)
+    #volume = float(ds.RescaleSlope) * volume + float(ds.RescaleIntercept)
     #print(ds.RescaleSlope, ds.RescaleIntercept)
-    # if any([item in dicom_dir for item in ['B1' , 'D1']]):
+    # if any([item in dicom_dir for item in ['D1', 'E2', 'F1', 'H2']]):
     #     # volumes2 = np.zeros_like(volume)
     #     # nslides = volume.shape[0]
     #     # for i in range(nslides):
     #     #     volumes2[i] = volume[nslides - i - 1]
     #     # volume = volumes2
-    #     np.flip(volume, axis=0)
-    if 'F1' in dicom_dir:
-        # Calculate the zoom factors for each dimension
-        zoom_factors = (343 / volume.shape[0], 1, 1)
-        # Resample the image
-        volume = zoom(volume, zoom_factors, order=3) 
+    #     volume = np.flip(volume, axis=0)
+    volume_flipped = flip_volume(volume)
+    volume = level_window(volume, 500, 3000)
     if numpy_format:
-        return [torch.tensor(volume.transpose(1, 2, 0)).to(device).float().unsqueeze(0).unsqueeze(0), volume]
+        return [torch.tensor(volume.transpose(1, 2, 0)).to(device).float().unsqueeze(0).unsqueeze(0), volume, 
+            torch.tensor(volume_flipped.transpose(1, 2, 0)).to(device).float().unsqueeze(0).unsqueeze(0), volume_flipped]
     else:
         # Now 'volume' contains the 3D volume of the DICOM slices
         # print(volume.shape)
@@ -89,8 +113,9 @@ def elstic_registration(fixed_image, moving_image, parameter_object):
     fixed_image = itk.GetImageFromArray(fixed_image)
     moving_image = itk.GetImageFromArray(moving_image)
     # Downsample the images by a factor of 4
-    fixed_image_down = itk.bin_shrink_image_filter(fixed_image, shrink_factors=[4, 4, 4])
-    moving_image_down = itk.bin_shrink_image_filter(moving_image, shrink_factors=[4, 4, 4])
+    downsample_factor = 4
+    fixed_image_down = itk.bin_shrink_image_filter(fixed_image, shrink_factors=[downsample_factor, downsample_factor, downsample_factor])
+    moving_image_down = itk.bin_shrink_image_filter(moving_image, shrink_factors=[downsample_factor, downsample_factor, downsample_factor])
     # Load Elastix Image Filter Object
     elastix_object = itk.ElastixRegistrationMethod.New(fixed_image_down, moving_image_down)
     elastix_object.SetParameterObject(parameter_object)
@@ -120,6 +145,11 @@ def find_shifts(img, gt, axis=-1, downsample=4):
     shift = np.argmin(rmses)*downsample
     return torch.roll(img, shifts=shift, dims=axis), shift, rmses
 
+def rolled_ssim(img1, img2):
+    ssim = metrics.SSIMMetric(spatial_dims=3, data_range=2000)
+    img2, _, _  = find_shifts(img2, img1, axis=-1, downsample=8)
+    return ssim(img1, img2)
+
 # Main Fucntion
 def main():
     psnr = metrics.PSNRMetric(max_val=2000)
@@ -140,12 +170,12 @@ def main():
     psrns_mean_matrix, ssims_mean_matrix, rmses_mean_matrix = zeros_matrix.copy(), zeros_matrix.copy(), zeros_matrix.copy()
     psrns_std_matrix, ssims_std_matrix, rmses_std_matrix = zeros_matrix.copy(), zeros_matrix.copy(), zeros_matrix.copy() 
     for i in range(len(scanners)):
-        print(scanners[i])
         for j in range(len(scanners)):
-            if i < j:
+            if i > j:
                 continue
-            files_scanner1 = sorted(glob(os.path.join(data_dir, scanners[i], f'*{dose}*{reconstruction_method}*')))
-            files_scanner2 = sorted(glob(os.path.join(data_dir, scanners[j], f'*{dose}*{reconstruction_method}*')))
+            print(scanners[i], 'compared with',scanners[j])
+            files_scanner1 = sorted(glob(os.path.join(data_dir, scanners[i], f'*{dose}*{reconstruction_method[0]}*')))
+            files_scanner2 = sorted(glob(os.path.join(data_dir, scanners[j], f'*{dose}*{reconstruction_method[0]}*')))
             if len(files_scanner1) != len(files_scanner2):
                 files_scanner1 = files_scanner1[:min([len(files_scanner1), len(files_scanner2)])]
                 files_scanner2 = files_scanner2[:min([len(files_scanner1), len(files_scanner2)])]
@@ -166,12 +196,22 @@ def main():
                 elif registration_mode.lower() == 'elastic':
                     img1 = read_dicom(file1, numpy_format=True)
                     img2 = read_dicom(file2, numpy_format=True)
-                    #_ssim = ssim(img1[0], img2[0]).item()
-                    #print(f'SSIM Before Registration {_ssim:0.4f}')
-                    #img2[0], shift, _ = find_shifts(img2[0], img1[0], axis=-1)
-                    #img2[1] = np.roll(img2[1], shift, axis=0)
-                    #_ssim = ssim(img1[0], img2[0]).item()
-                    #print(f'SSIM Before Registration {_ssim:0.4f}')
+                    _ssim0 = rolled_ssim(img1[0], img2[0]).item()
+                    _ssim1 = rolled_ssim(img1[0], img2[2]).item()
+                    if _ssim1 > _ssim0:
+                        img2[0] = img2[2]
+                        img2[1] = img2[3]
+                        print('Axial dimension flipped.')
+                    _ssim = min(_ssim0, _ssim1)
+                    #_ssim = ssim(level_window_torch(img1[0], -500, 1000), level_window_torch(img2[0], -500, 1000)).item()
+                    print(f'SSIM Before Rolling {_ssim:0.4f}')
+                    img2[0], shift, _ = find_shifts(img2[0], img1[0], axis=-1)
+                    img2[1] = np.roll(img2[1], shift, axis=0)
+                    _ssim = ssim(img1[0], img2[0]).item()
+                    print(f'SSIM Before Registration {_ssim:0.4f}')
+                    # plt.hist(img2[1].flatten());plt.hist(img1[1].flatten());plt.savefig('fig.png');plt.close()
+                    # idx=172;plt.imshow(np.clip(img1[1], -500, 1000)[idx,...], 'gray');plt.savefig('fig.png');plt.imshow(np.clip(img2[1], -500, 1000)[idx,...], 'gray');plt.savefig('fig2.png');
+
                     img1, img2, _ = elstic_registration(img1[1], img2[1], parameter_object)
                     img1, img2 = elastic_results_to_tensor(img1, img2)
 
@@ -188,22 +228,21 @@ def main():
                 
                 _psnr = psnr(img1, img2).item()
                 _ssim = ssim(img1, img2).item()
-                #if _ssim < 0.8:
-                #    print('Not Again!!!')
                 _rmse = rmse(img1, img2).item()
                 #if _ssim > 0.8:
                 psrns.append(_psnr)
                 ssims.append(_ssim)
                 rmses.append(_rmse)
-                print(_ssim)
+                print(f'{_ssim:0.4f}')
             if len(psrns) == 0:
                 psrns.append([-1, -1, -1])
                 ssims.append([-1, -1, -1])
                 rmses.append([-1, -1, -1])
-            psrns_mean_matrix[i, j] = np.median(np.array(psrns))
-            ssims_mean_matrix[i, j] = np.median(np.array(ssims))
-            rmses_mean_matrix[i, j] = np.median(np.array(rmses))
-            psrns_std_matrix[i, j] = np.std(np.array(psrns))
+            # Metri[i,j] = Metrci[j,i], However it is easier to debug this way:
+            psrns_mean_matrix[j, i] = np.median(np.array(psrns))
+            ssims_mean_matrix[j, i] = np.median(np.array(ssims))
+            rmses_mean_matrix[j, i] = np.median(np.array(rmses))
+            psrns_std_matrix[j, i] = np.std(np.array(psrns))
             ssims_std_matrix[i, j] = np.std(np.array(ssims))
             rmses_std_matrix[i, j] = np.std(np.array(rmses))
 
